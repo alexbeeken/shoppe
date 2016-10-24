2a09bbed4f6ee7ca3572ffb8efc562787a6f353f
Alex Beeken <abeeken@focusvision.com>
2016-10-23 23:37:44 -0700

    change root to merch

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
+
   mount Shoppe::Engine => "/shoppe"
+  get '/merch', to: 'products#index'
 
   get "product/:permalink", to: "products#show", as: "product"
   post "product/:permalink", to: "products#buy", as: "buy"
