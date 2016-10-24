06d9a41f96adfcfb7a7023cc424231854261f5da
Alex Beeken <abeeken@focusvision.com>
2016-10-23 23:04:40 -0700

    first commit

---
+++ config/routes.rb
@@ -0,0 +1,14 @@
+Rails.application.routes.draw do
+  mount Shoppe::Engine => "/shoppe"
+
+  get "product/:permalink", to: "products#show", as: "product"
+  post "product/:permalink", to: "products#buy", as: "buy"
+  root to: "products#index"
+
+  delete "basket", to: "orders#destroy"
+  get "basket", to: "orders#show"
+
+  match "checkout", to: "orders#checkout", as: "checkout", via: [:get, :patch]
+  match "checkout/pay", to: "orders#payment", as: "checkout_payment", via: [:get, :post]
+  match "checkout/confirm", to: "orders#confirmation", as: "checkout_confirmation", via: [:get, :post]
+end
