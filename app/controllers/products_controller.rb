f24a4b6405f9d01de2efb43c373467d3f48c56f7
Alex Beeken <abeeken@focusvision.com>
2016-10-28 20:32:16 -0700

    add flash messages and handle some errors

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -8,9 +8,18 @@ class ProductsController < ApplicationController
   end
 
   def buy
-    @product = Shoppe::Product.root.find_by_permalink!(params[:permalink])
-    @quantity = params['quantity'] || 1
-    current_order.order_items.add_item(@product, @quantity.to_i)
-    redirect_to merch_path, :notice => "Product has been added successfuly!"
+    begin
+      @product = Shoppe::Product.root.find_by_permalink!(params[:permalink])
+      @quantity = params['quantity'] || 1
+      current_order.order_items.add_item(@product, @quantity.to_i)
+      flash[:success] = "#{@quantity} x #{@product.name} has been added to basket!"
+      redirect_to product_path(@product.permalink)
+    rescue Shoppe::Errors::NotEnoughStock => e
+      flash[:alert] = "Sorry, #{@product.name} is sold out or not available in that quantity."
+      redirect_to product_path(@product.permalink)
+    rescue Shoppe::Errors::UnorderableItem => e
+      flash[:alert] = "Sorry, an error has occured. Please contact musiccramsessions@gmail.com for help."
+      redirect_to product_path(@product.permalink)
+    end
   end
 end
