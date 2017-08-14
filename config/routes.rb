c66cf93a6ff79b702047588ced513873a7cd2963
Alex Beeken <aeb242@gmail.com>
2017-08-14 12:25:20 -0700

    add about route and style nav menu more manually

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
   mount Shoppe::Engine => "/shoppe"
   root to: 'products#index'
   get '/merch', to: 'products#index'
+  get '/about', to: 'about#index'
 
   get "product/:permalink", to: "products#show", as: "product"
   post "product/:permalink", to: "products#buy", as: "buy"
