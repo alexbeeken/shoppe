7b361656139becec42aa3ba537c6f747b33f5788
Alex Beeken <aeb242@gmail.com>
2017-08-15 23:47:01 -0700

    fix some bugs with variants

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -10,12 +10,16 @@ class ProductsController < ApplicationController
 
   def buy
     handle_errors do
-      @product = Shoppe::Product.root.find_by_permalink!(params[:permalink])
+      @product = if params[:variant]
+                   Shoppe::Product.find(params[:variant].to_i)
+                 else
+                   Shoppe::Product.root.find_by_permalink!(params[:permalink])
+                 end
       @quantity = params['quantity'].to_i
-      raise Shoppe::Errors::NotEnoughStock unless quantity_valid?
+      raise Shoppe::Errors::NotEnoughStock unless quantity_valid?(@product, @quantity)
       current_order.order_items.add_item(@product, @quantity)
       flash[:success] = "#{@quantity} x #{@product.name} has been added to basket!"
-      redirect_to product_path(@product.permalink)
+      redirect_to '/store'
     end
   end
 
@@ -41,9 +45,9 @@ class ProductsController < ApplicationController
     end
   end
 
-  def quantity_valid?
-    stock = @product.stock
+  def quantity_valid?(product, quantity)
+    stock = product.stock
     return false if stock < 1
-    @quantity.between?(1, stock)
+    quantity.between?(1, stock)
   end
 end
