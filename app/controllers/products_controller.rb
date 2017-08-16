0c38d36b0944354c8efc7b2fb9201fdf9e84558b
Alex Beeken <aeb242@gmail.com>
2017-08-15 23:57:51 -0700

    fix variant ordering bug and make items white instead of yellow

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -10,16 +10,20 @@ class ProductsController < ApplicationController
 
   def buy
     handle_errors do
-      @product = if params[:variant]
-                   Shoppe::Product.find(params[:variant].to_i)
-                 else
-                   Shoppe::Product.root.find_by_permalink!(params[:permalink])
-                 end
+      @product = Shoppe::Product.root.find_by_permalink!(params[:permalink])
+      @variant = Shoppe::Product.find(params[:variant].to_i) if params[:variant]
       @quantity = params['quantity'].to_i
-      raise Shoppe::Errors::NotEnoughStock unless quantity_valid?(@product, @quantity)
-      current_order.order_items.add_item(@product, @quantity)
-      flash[:success] = "#{@quantity} x #{@product.name} has been added to basket!"
-      redirect_to '/store'
+      if @variant
+        raise Shoppe::Errors::NotEnoughStock unless quantity_valid?(@variant, @quantity)
+        current_order.order_items.add_item(@variant, @quantity)
+        flash[:success] = "#{@quantity} x #{@variant.name} #{@product.name} has been added to basket!"
+        redirect_to '/store'
+      else
+        raise Shoppe::Errors::NotEnoughStock unless quantity_valid?(@product, @quantity)
+        current_order.order_items.add_item(@product, @quantity)
+        flash[:success] = "#{@quantity} x #{@product.name} has been added to basket!"
+        redirect_to product_path(@product.permalink)
+      end
     end
   end
 
@@ -38,10 +42,18 @@ class ProductsController < ApplicationController
       block.call
     rescue Shoppe::Errors::NotEnoughStock => e
       flash[:alert] = "Sorry, #{@product.name} is sold out or not available in that quantity."
-      redirect_to product_path(@product.permalink)
+      if @variant
+        redirect_to '/store'
+      else
+        redirect_to product_path(@product.permalink)
+      end
     rescue Shoppe::Errors::UnorderableItem => e
       flash[:alert] = "Sorry, an error has occured. Please contact musiccramsessions@gmail.com for help."
-      redirect_to product_path(@product.permalink)
+      if @variant
+        redirect_to '/store'
+      else
+        redirect_to product_path(@product.permalink)
+      end
     end
   end
 
